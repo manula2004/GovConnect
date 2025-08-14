808451105757c51d600f0c369a919eae9faf799a
Manula Thejan <thejanmanula@gmail.com>
2025-08-14 16:47:05 +0530

    initial commit - created the basic file structure with blank files

---
+++ src/pages/Citizen/ComplaintForm.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function ComplaintForm() {
+    return (
+        <div>ComplaintForm page</div>
+    );
+}
---
+++ src/pages/Citizen/PassportServices.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function PassportServices() {
+    return (
+        <div>PassportServices page</div>
+    );
+}
