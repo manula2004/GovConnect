c862b5cb3342c7bafbb1643776a3f4c36ee22a2d
Manula Thejan <thejanmanula@gmail.com>
2025-08-15 17:05:01 +0530

    implementing the form stuff. halfway thru making the login page

---
+++ src/components/InputWithIcon.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import EmailIcon from '../assets/Icons/EmailIcon.jsx';
+import './InputWithIcon.css';
+
+const InputWithIcon = ({ 
+  type = 'text',
+  placeholder = '',
+  iconColor = '#033EA1',
+  iconPosition = 'right',
+  ...inputProps 
+}) => {
+  return (
+    <div className={`input-with-icon ${iconPosition}-icon`}>
+      <input
+        type={type}
+        placeholder={placeholder}
+        className="form-input"
+        {...inputProps}
+      />
+      <span className="input-icon" style={{ color: iconColor }}>
+        <EmailIcon />
+      </span>
+    </div>
+  );
+};
+
+InputWithIcon.propTypes = {
+  type: PropTypes.string,
+  placeholder: PropTypes.string,
+  iconColor: PropTypes.string,
+  iconPosition: PropTypes.oneOf(['left', 'right']),
+};
+
+export default InputWithIcon;
