4a30ea27d4642d24b735e3de446d9fd08d9d7dce
Manula Thejan <thejanmanula@gmail.com>
2025-08-15 12:05:44 +0530

    added logo component, updated style.css for better maintainability, made buttons vertical

--- src/pages/Common/LanguageSelection.jsx
+++ src/pages/Common/LanguageSelection.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Button from "../../components/Button";
 import { useNavigate } from "react-router-dom";
+import Logo from "../../components/Logo";
 
 export default function LanguageSelection() {
   const navigate = useNavigate();
@@ -10,13 +11,24 @@ export default function LanguageSelection() {
   };
 
   return (
-    <div style={{ textAlign: "center", marginTop: "5rem" }}>
-      <h1>Select Your Language</h1>
-      <div style={{ marginTop: "2rem", display: "flex", gap: "1rem", justifyContent: "center" }}>
-        <Button label="සිංහල" onClick={handleLanguageSelect} />
-        <Button label="English" onClick={handleLanguageSelect} />
-        <Button label="தமிழ்" onClick={handleLanguageSelect} />
-      </div>
-    </div>
+<div style={{ 
+  textAlign: "center", 
+  marginTop: "6rem", 
+  padding: "0 1rem" // padding so buttons don't touch screen edges
+}}>
+    <Logo width="250px" />
+  <h1 style={{ fontSize: "1rem" , marginTop: "-1rem"}}>Select Your Language</h1>
+  <div style={{ 
+    marginTop: "1rem", 
+    display: "flex", 
+    flexDirection: "column", 
+    gap: "1rem", 
+    alignItems: "center" 
+  }}>
+    <Button label="සිංහල" onClick={handleLanguageSelect} />
+    <Button label="தமிழ்" onClick={handleLanguageSelect} />
+    <Button label="English" onClick={handleLanguageSelect} /> 
+  </div>
+</div>
   );
 }
